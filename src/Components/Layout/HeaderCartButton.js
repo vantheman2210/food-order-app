c53c01e4fb971643181bf31f91926cb83a61dee3
vantheman2210 <martinovic.ba@gmail.com>
2023-05-15 12:42:57 +0200

    Creating Header Cart Button component

---
+++ src/Components/Layout/HeaderCartButton.js
@@ -0,0 +1,7 @@
+import classes from './HeaderCartButton.module.css'
+
+const HeaderCartButton = props => { 
+  return <button className={classes.button}></button>
+};
+
+export default HeaderCartButton;
