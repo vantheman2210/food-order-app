551afc9b0c47502ad48a8fbf5ddeae4a39f0ef5d
vantheman2210 <martinovic.ba@gmail.com>
2023-05-15 12:46:56 +0200

    Adding cart icon component

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,7 +1,11 @@
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = props => { 
-  return <button className={classes.button}></button>
+  return <button className={classes.button}>
+    <span></span>
+    <span></span>
+    <span></span>
+  </button>
 };
 
 export default HeaderCartButton;
