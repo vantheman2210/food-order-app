3c7c2b649a7d20f793bea115970b456e6ea175e4
vantheman2210 <martinovic.ba@gmail.com>
2023-05-15 13:18:29 +0200

    Finished meal item component

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -34,7 +35,14 @@ const AvailableMeals = () => {
       <Card>
         <ul>
           {DUMMY_MEALS.map((meal) => {
-            return <li key={meal.id}>{meal.name}</li>;
+            return (
+              <MealItem
+                key={meal.id}
+                price={meal.price}
+                name={meal.name}
+                description={meal.description}
+              />
+            );
           })}
         </ul>
       </Card>
