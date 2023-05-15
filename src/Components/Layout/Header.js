5a0b3164d9df543482312de2bfcc3669967eb41d
vantheman2210 <martinovic.ba@gmail.com>
2023-05-15 12:37:51 +0200

    Applied css styles with css modules

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,19 +1,19 @@
 import React from "react";
-import './Header.module.css';
-import mealsIMG from '../../Assets/meals.jpg';
+import classes from "./Header.module.css";
+import mealsIMG from "../../Assets/meals.jpg";
 
 const Header = (props) => {
   return (
     <React.Fragment>
-      <header>
+      <header className={classes.header}>
         <h1>Meal-Order</h1>
         <button>Cart</button>
       </header>
-      <div>
-    <img></img>
+      <div className={classes["main-image"]}>
+        <img src={mealsIMG} alt="A table full of food"></img>
       </div>
     </React.Fragment>
-  )
+  );
 };
 
 export default Header;
