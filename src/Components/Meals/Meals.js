f5755f4f81bb1556f860cccd7b19f5d4eddb8c6c
vantheman2210 <martinovic.ba@gmail.com>
2023-05-15 13:09:03 +0200

    Finished card

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import MealsSummary from "./MealsSummary";
 import AvailableMeals from "./AvailableMeals";
+import Card from "../UI/Card";
 
 const Meals = () => {
   return (
