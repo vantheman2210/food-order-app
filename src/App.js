f47eed99baea74021ae3e0230778c7f94eb4e284
vantheman2210 <martinovic.ba@gmail.com>
2023-05-16 12:22:51 +0200

    Finished hiding cart modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import Cart from "./Components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+const shownCartHandler = () => { 
+  setCartIsShown(true);
+};
+
+const hideCartHandler = () => { 
+  setCartIsShown(false);
+}
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onHideCart={hideCartHandler}/>}
+      <Header onShowCart={shownCartHandler}/>
       <main>
         <Meals />
       </main>
--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,26 +1,26 @@
 import classes from "./Cart.module.css";
-
+import Modal from "../UI/Modal";
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
+    <Modal onClose={props.onHideCart}>
       {cartItems}
       <div className={classes.total}>
-        <span>Total AMount</span>
+        <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button-alt"]}>Close</button>
+        <button className={classes["button-alt"]} onClick={props.onHideCart}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
